8953b773437b59fdf09040a51d0f12078634c7c6
Mamta <120008345+Mamta13kunal@users.noreply.github.com>
2023-08-26 17:17:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+let name=document.getElementById("form1");
+let fname=name.fname.value;
+	let lname=name.lname.value;
 
+	alert(fname+" "+ lname);
 }
